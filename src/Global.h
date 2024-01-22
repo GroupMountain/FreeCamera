0364cd8e34dff39b57084e7db40d6e38b2463240
Tsubasa6848 <116721335+Tsubasa6848@users.noreply.github.com>
2024-01-22 21:49:37 +0800

    refactor: optimize language

--- src/Global.h
+++ src/Global.h
@@ -1,19 +1,19 @@
 #pragma once
 #include "Plugin.h"
-#include <GMLIB/Server/BinaryStreamAPI.h>
 #include <GMLIB/Files/JsonLanguage.h>
+#include <GMLIB/Server/BinaryStreamAPI.h>
 #define PLUGIN_NAME "FreeCamera"
 
 using namespace GMLIB;
 
-extern ll::Logger logger;
+extern ll::Logger                 logger;
 extern std::unordered_set<uint64> FreeCamList;
-extern void RegisterCommand();
-extern void initLanguage();
+extern void                       RegisterCommand();
+extern void                       initLanguage();
 
 extern std::string tr(std::string key);
 
 namespace FreeCamera {
-    extern void DisableFreeCamera(Player* pl);
-    extern void EnableFreeCamera(Player* pl);
-}
+extern void DisableFreeCamera(Player* pl);
+extern void EnableFreeCamera(Player* pl);
+} // namespace FreeCamera
