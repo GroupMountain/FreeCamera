88ed92e7a82a6c9906e6260c302ff7a8ec6b1618
Tsubasa6848 <116721335+Tsubasa6848@users.noreply.github.com>
2024-02-09 14:49:54 +0800

    fix: fix alias

--- src/Command.cpp
+++ src/Command.cpp
@@ -8,6 +8,7 @@ using namespace ll::command;
 void RegisterCommand() {
     auto& cmd = CommandRegistrar::getInstance()
                     .getOrCreateCommand("freecamera", tr("freecamera.command.desc"), CommandPermissionLevel::Any);
+    ll::service::getCommandRegistry()->registerAlias("freecamera", "fc");
     cmd.overload().execute<[&](CommandOrigin const& origin, CommandOutput& output) {
         auto entity = (GMLIB_Actor*)origin.getEntity();
         if (entity && entity->isPlayer()) {
