b7183918f05bf713908e8cc31627e1148598c06b
子沐呀 <163636894+zimuya4153@users.noreply.github.com>
2025-01-31 20:23:49 +0800

    fix: fix version

--- xmake.lua
+++ xmake.lua
@@ -8,9 +8,9 @@ if not has_config("vs_runtime") then
 end
 
 -- Option 1: Use the latest version of LeviLamina released on GitHub.
-add_requires("levilamina")
-add_requires("levibuildscript")
-add_requires("gmlib")
+add_requires("levilamina 1.0.0")
+add_requires("levibuildscript 0.3.0")
+add_requires("gmlib 0.13.10")
 
 target("FreeCamera") -- Change this to your mod name.
     add_cxflags(
@@ -37,7 +37,7 @@ target("FreeCamera") -- Change this to your mod name.
     add_rules("@levibuildscript/linkrule")
     set_exceptions("none")
     set_kind("shared")
-    set_languages("cxx23")
+    set_languages("cxx20")
 
     after_build(function (target)
         local mod_packer = import("scripts.after_build")
